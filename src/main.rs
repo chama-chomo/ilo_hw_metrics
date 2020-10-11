e93819858f7b990b80bbd090220f005535ce1e75
Peter Kuchar <peter.kuchar@konicaminolta.cz>
2020-10-11 10:17:04 +0200

    Added cli args for ILO user/passw

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
 use std::process::Command;
 use ilo_hw_metrics::ilo_api_mod::IloSession;
+use clap::{Arg, App};
 
 
 #[derive(Debug)]
@@ -48,15 +49,40 @@ impl CommandIpmi<'_> {
     }
 }
 
+fn cli_parse() -> Result<clap::ArgMatches<'static>, ()> {
+    let matches = App::new("HP Machine status")
+        .version("0.1.0")
+        .author("chama-chomo")
+        .about("Retrieves HP machine status via ILO API")
+        .arg(Arg::with_name("user")
+                 .short("u")
+                 .long("user")
+                 .takes_value(true)
+                 .help("ILO user"))
+        .arg(Arg::with_name("passw")
+                 .short("p")
+                 .long("password")
+                 .takes_value(true)
+                 .help("ILO passwd"))
+        .get_matches();
+
+
+    Ok(matches)
+}
+
 fn main() {
+
+    // pars cli args
+    let args =  cli_parse().expect("Failed to obtain cli args");
+    let user = args.value_of("user").unwrap_or("Administrator");
+    let passwd = args.value_of("passw").unwrap();
+
     let ilo_status = CommandIpmi::run().get_ilo_status();
     let ilo_ip = ilo_status.build_ilo_params();
 
     // Create ILO session
     let url: &str =  &("https://".to_owned() + &ilo_ip.ip_address);
-    let user: &str = "Administrator";
-    let passw: &str = "Administrator";
-    let init_session = IloSession::create(url, user, passw).expect("Token not acquired");
+    let init_session = IloSession::create(url, &user, &passwd).expect("Token not acquired");
 
     let chassis_data = init_session.chassis().expect("Chassis data could not be obtained");
 
